5a49ab7170fb8707b604a2d9eb636657d04e7ffb
hossein bahak <34401089+hosseinbahak@users.noreply.github.com>
2023-07-26 00:25:53 +0330

    Update admin.py

--- panel/admin.py
+++ panel/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Project
+from .models import *
 from import_export.admin import ImportExportModelAdmin
 
 @admin.register(Project)
 class ProjectAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('id', 'employee', 'employer', 'check_date', 'state')
     list_filter = ('employee', 'state')
+
+@admin.register(ProjectCheckDateHistory)
+class ProjectCheckDateHistoryAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('id', 'timestamp')
+
 
